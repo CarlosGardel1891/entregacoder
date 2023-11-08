d96ae59a85c4277e3903bade6f2b0b94786980cc
CarlosGardel1891 <MC251514@fi365.ort.edu.uy>
2023-11-08 16:38:04 -0300

    funcionando, no terminado

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Curso
 
 # Create your views here.
+
+def home(request):
+    cursos = Curso.objects.all()
+    return render(request, "gCursos.html", {"cursos": cursos})
+
